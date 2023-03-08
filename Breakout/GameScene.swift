391f633563f11d2f08fe0730e1610b3fd968de97
sol gudmin <115568958+schnappiiiiiiii@users.noreply.github.com>
2023-03-08 11:08:40 -0600

    created ball object (although it doesn't currently show up

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -9,9 +9,12 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
+    var ball = SKShapeNode()
     override func didMove(to view: SKView) {
         createBackground()
+    }
+    func resetGame(){
+        makeBall()
     }
     func createBackground(){
         let stars = SKTexture(imageNamed: "Stars")
@@ -27,4 +30,30 @@ class GameScene: SKScene {
             starsBackground.run(moveForever)
         }
     }
+    func makeBall(){
+        ball.removeFromParent()
+        ball = SKShapeNode(circleOfRadius: 10)
+        ball.position = CGPoint(x: frame.midX, y: frame.midY)
+        ball.strokeColor = .black
+        ball.fillColor = .yellow
+        ball.name = "ball"
+        
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+        
+        ball.physicsBody?.isDynamic = false
+        
+        ball.physicsBody?.usesPreciseCollisionDetection = true
+        
+        ball.physicsBody?.friction = 0
+        
+        ball.physicsBody?.affectedByGravity = false
+        
+        ball.physicsBody?.restitution = 1
+        
+        ball.physicsBody?.linearDamping = 0
+        ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
+        
+        addChild(ball)
+    }
+    
 }

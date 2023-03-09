89e170c9bd5359c66ec539fe4948462bda497c87
sol gudmin <115568958+schnappiiiiiiii@users.noreply.github.com>
2023-03-09 11:13:44 -0600

    created ball dynamic behavior (BUT IT STILL WONT SHOW UP)

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -8,13 +8,15 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     var ball = SKShapeNode()
     var paddle = SKSpriteNode()
     var brick = SKSpriteNode()
     var loseZone = SKSpriteNode()
     
     override func didMove(to view: SKView) {
+        physicsWorld.contactDelegate = self
+        self.physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         createBackground()
     }
     func resetGame(){
@@ -22,6 +24,10 @@ class GameScene: SKScene {
         makePaddle()
         makeBrick()
         makeLoseZone()
+    }
+    func kickBall(){
+        ball.physicsBody?.isDynamic = true
+        ball.physicsBody?.applyImpulse(CGVector(dx: 3, dy: 5))
     }
     func createBackground(){
         let stars = SKTexture(imageNamed: "Stars")
